9cc93a35fadefe0ea63c65098a87a47e3ff31f4b
carpiodes07-dot <carpiodes07@gmail.com>
2026-04-13 18:29:21 +0200

    Add files via upload

---
+++ Persona.java
@@ -0,0 +1,15 @@
+public class Persona {
+
+    String nome = "Luca";
+    String cognome = "Rossi";
+    int eta = 25;
+    String colorepreferito = "Blu";
+
+    void saluta() {
+        System.out.println("Ciao sono " + nome);
+    }
+
+    void cammina() {
+        System.out.println("Sto camminando...");
+    }
+}
